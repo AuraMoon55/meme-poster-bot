97b07bf16c6d4e67e587416e5be585f2966c96f0
Anshul Garg <garganshul553@gmail.com>
2021-12-23 18:44:08 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -9,7 +9,7 @@ from demcor import sudo_users_only
 bot = Client('bot',api_id=API_ID,api_hash=API_HASH,bot_token=TOKEN)
 
 
-@bot.on_message(filters.command['start', 'start@VGW_bot'])
+@bot.on_message(filters.command('start'))
 def main(client, message):
     text = f"Hey there {message.from_user.id}, I am meme poster bot which uploads memes from reddit.\nI work for @Anime_Meme_Kingdom Only"
     buttons = InlineKeyboardMarkup(
@@ -31,7 +31,7 @@ async def faqs(_, query):
     texto = """ Im just a nub bot made by @Villainevil_Support"""
     await query.message.edit(text=texto)
 
-@bot.on_message(filters.command['post', 'post@VGW_bot'], filters.user(SUDO_USERS))
+@bot.on_message(filters.command('post'), filters.user(SUDO_USERS))
 @sudo_users_only
 async def post(_, message):
     while True:
@@ -45,7 +45,7 @@ async def post(_, message):
         await bot.send_photo(chat_id=CHANNEL, photo=meme, caption=f"#offtopic #meme {title} \n \n @Anime_meme_kingdom")
         time.sleep(10)
 
-@bot.on_message(filters.command['post_here', 'post_here@VGW_bot'], filters.user(SUDO_USERS))
+@bot.on_message(filters.command('post_here'), filters.user(SUDO_USERS))
 @sudo_users_only
 async def post(_, message):
     while True:
