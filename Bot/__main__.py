5b2da6ac361abee3f6b4dea79890f8c74edf44cc
Anshul Garg <garganshul553@gmail.com>
2021-12-23 17:21:04 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 import requests as req
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from config import *
 from demcor import sudo_users_only
 
@@ -11,7 +12,24 @@ bot = Client('bot',api_id=API_ID,api_hash=API_HASH,bot_token=TOKEN)
 @bot.on_message(filters.command('start'))
 def main(client, message):
     text = f"~Shoo Shoo~ Go Away.\nI work for @Anime_Meme_Kingdom Only"
-    message.reply_text(text=text)
+    buttons = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="Meme Channel", url = "t.me/Anime_Meme_Kingdom"
+            ),
+            InlineKeyboardButton(
+                text="FAQs", callback_data="faqsinline"
+            ),
+        ],
+    ]
+)
+    message.reply_text(text=text, reply_markup=buttons)
+
+@bot.on_callback_query(filters.regex("faqsinline"))
+async def faqs(_, query):
+    texto = """ Im just a nub bot made by @Villainevil_Support"""
+    await query.message.edit(text=texto)
 
 @bot.on_message(filters.command('post'))
 @sudo_users_only
@@ -24,7 +42,7 @@ async def post(_, message):
             x = x.json()
         title=x.get(str("title"))
         meme=x.get(str("url"))
-        await bot.send_photo(chat_id=CHANNEL, photo=meme, caption=f"#meme {title} \n \n @Anime_meme_kingdom")
+        await bot.send_photo(chat_id=CHANNEL, photo=meme, caption=f"#offtopic #meme {title} \n \n @Anime_meme_kingdom")
         time.sleep(360)
         
 bot.run()
