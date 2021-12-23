974bb9a54a00572414bec6e4aff1ab72f0b445ac
Anshul Garg <garganshul553@gmail.com>
2021-12-23 16:34:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import os
 import random
+from os import getenv
+from dotenv import load_dotenv
 
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
 TOKEN = "5015207309:AAE1Hf1L1zpJ-YNxNcbP0AooJwu29DT-0So"
 API_ID = 2919867
 API_HASH = "90dd95178a8d13a69bfdbc7da68d23a4"
 CHANNEL = -1001523814336
+SUDO_USERS = SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
 SUBREDS = [
     "dankmemes",
